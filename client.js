986dcaf5263ab0aa750a0d70975041e5ed67ce51
Juan Esteban <esteban.inova@gmail.com>
2020-05-03 13:29:38 -0500

    Initial commit

---
+++ client.js
@@ -0,0 +1,57 @@
+var socket = new JsSIP.WebSocketInterface('wss://192.168.1.7:8089/ws');
+var configuration = {
+  sockets  : [ socket ],
+  uri      : 'sip:1060@192.168.1.7:5060',
+  password : 'marin1234'
+};
+
+var coolPhone = new JsSIP.UA(configuration);
+
+coolPhone.start();
+
+coolPhone.on('connected', function(e){
+    console.log("conectado")
+    console.log(e)
+});
+
+coolPhone.on('registered', function(e){
+    console.log("REGISTERED") 
+    console.log(e) 
+
+});
+coolPhone.on('unregistered', function(e){
+    console.log("Unregistered")  
+    console.log(e) 
+});
+coolPhone.on('registrationFailed', function(e){ 
+    console.log("REGISTERED failed") 
+    console.log(e) 
+});
+
+
+
+// Register callbacks to desired call events
+var eventHandlers = {
+    'progress': function(e) {
+      console.log('call is in progress');
+    },
+    'failed': function(e) {
+        console.log(e)
+        console.log('call failed with cause: '+ e.cause);
+    },
+    'ended': function(e) {
+      console.log('call ended with cause: '+ e.data.cause);
+    },
+    'confirmed': function(e) {
+      console.log('call confirmed');
+    }
+  };
+  
+  var options = {
+    'eventHandlers'    : eventHandlers,
+    'mediaConstraints' : { 'audio': true, 'video': false }
+  };
+
+  var text = 'Hello Bob!';
+
+  coolPhone.sendMessage('sip:1061@192.168.1.7:5060', text);
